94b61eba7ed0c5b32c9ea458fddf25da85eccb94
Raed Salah Bzour <raedbzour77@gmail.com>
2024-04-21 00:25:32 +0200

    adding cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { CartModule } from './cart/cart.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ProductModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ProductModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    CartModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
