7bf41dc270a3ad435586d49f6a0ef0c057bbfd15
Raed Salah Bzour <raedbzour77@gmail.com>
2024-04-22 00:28:16 +0200

    finishing the app

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -8,10 +8,25 @@ import { Product } from 'src/app/models/product';
 })
 export class CartViewComponent implements OnInit {
   cartItems: Product[] = [];
+  totalPrice: number = 0;
   constructor(private cartService: CartService) {}
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe((data) => {
       this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
     });
+  }
+  getTotalPrice(): number {
+    let total = 0;
+    for (let i of this.cartItems) {
+      total += i.price;
+    }
+    return total;
+  }
+  clearCart(): void {
+    this.cartService.clearCart().subscribe();
+  }
+  checkout(): void {
+    this.cartService.checkout(this.cartItems).subscribe();
   }
 }
--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -8,10 +10,42 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  constructor(private productService: ProductService) {}
+  filteredProducts: Product[] = [];
+  sortOrder: string = '';
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private snackBar: MatSnackBar
+  ) {}
   ngOnInit(): void {
     this.productService.getProducts().subscribe((data) => {
       this.products = data;
+      this.filteredProducts = data;
     });
+  }
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product).subscribe({
+      next: () => {
+        this.snackBar.open('added to cart', '', {
+          duration: 2000,
+        });
+      },
+    });
+  }
+  applyFilter(event: Event): void {
+    let serchTerm = (event.target as HTMLInputElement).value;
+    serchTerm = serchTerm.toLowerCase();
+    this.filteredProducts = this.products.filter((product) =>
+      product.name.toLowerCase().startsWith(serchTerm)
+    );
+    this.sortProducts(this.sortOrder);
+  }
+  sortProducts(sortValue: string) {
+    let sortOrder = sortValue;
+    if (sortOrder === 'priceLowHigh') {
+      this.filteredProducts.sort((a, b) => a.price - b.price);
+    } else if (sortOrder === 'priceHighLow') {
+      this.filteredProducts.sort((a, b) => b.price - a.price);
+    }
   }
 }
